d90d656f1fbe9e3cd88d9f66b62c04a18db3f515
Claver <hello@soft-angel.ru>
2025-10-09 21:07:36 +0300

    Change token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   backendApiUrl: 'https://app-apm-internal-api-test-1.azurewebsites.net/api/v1',
-  stripePublicKey: 'pk_test_51Rv3WqBhuZitcQp8G4L840yi5jTKp47HdFClwyPsf1MJ4Ane3I9db8bZFVFp15Nx5qRqTzTfezJ5EHOvRvV8FxS100u3hRkjzJ'
+  stripePublicKey: 'pk_test_51SGJFSACT5aIKe7L5pvhzJNwrvwRueJDkLVYtXZNcg4z8YiwIebbyXyj8Ioce6o1Ca1okHrzXWP9m74iNHzKqK1B00q8AneFG1'
 };
